8f23f2d0ae1a1a5fe0ed71896441b412aa7479f2
juan.parra <jdparrapimiento@gmail.com>
2019-08-26 19:37:49 -0500

    configure google map key in appdelegate

---
+++ MiAguilaPrueba/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MiAguilaPrueba
+//
+//  Created by Juan David Parra Pimiento on 8/26/19.
+//  Copyright © 2019 Juan David Parra Pimiento. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let googleMapKey = "AIzaSyAiHn-8ZDAYORWZPj64HEJwEWBxS7HKz_4"
+}
